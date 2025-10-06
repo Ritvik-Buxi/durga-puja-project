f413e150239630b0cac33480c18e4e7a71e704ba
Ritvik Buxi <66825562+Ritvik-Buxi@users.noreply.github.com>
2025-10-06 12:36:54 +0530

    Update expression.py

--- expression.py
+++ expression.py
@@ -1,4 +1,4 @@
 x = int(input("Enter any number x : "))
 y = int(input("Enter any number y : "))
 z = int(input("Enter any number z : "))
-print(f"Your expression for ")
+print(f"The value for expression for 4x⁴+3y⁵+9z is {4*x**4+3*y**5+9*z}")
