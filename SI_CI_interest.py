df6e7a6b887a198d1d5ae65e243be8754a945450
Ritvik Buxi <66825562+Ritvik-Buxi@users.noreply.github.com>
2025-10-06 11:44:17 +0530

    Create SI_CI_interest.py

---
+++ SI_CI_interest.py
@@ -0,0 +1,8 @@
+prncpl_amnt = float(input("Enter your principal amount: "))
+rate = float(input("Enter your rate percentage: "))
+time_taken = float(input("Enter the total time taken to hold loan(in years): "))
+
+S_I = (prncpl_amnt*rate*time_taken)/100
+C_I = prncpl_amnt*(((rate+100)/100)**time_taken)
+
+print("Your Simple interest is ",S_I,"Your Compound Interest is ",C_I)
