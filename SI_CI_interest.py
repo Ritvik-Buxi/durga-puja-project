fcb65e3e25de41247a09fb64b96a4f09bf0d1ccf
Ritvik Buxi <66825562+Ritvik-Buxi@users.noreply.github.com>
2025-10-09 18:07:17 +0530

    corrected

--- SI_CI_interest.py
+++ SI_CI_interest.py
@@ -5,4 +5,4 @@ time_taken = float(input("Enter the total time taken to hold loan(in years): "))
 S_I = (prncpl_amnt*rate*time_taken)/100
 C_I = prncpl_amnt*(((rate+100)/100)**time_taken)
 
-print("Your Simple interest is ",S_I,"Your Compound Interest is ",C_I)
+print("Your Simple interest is ",S_I,"Your Compound Interest is ",C_I-prncpl_amnt)
